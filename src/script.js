4a0e664d909670838a3e690ad3b10e9165533872
tinameldahl <tina@tinameldahl.no>
2021-05-09 21:56:13 +0200

    Forecast connected API

--- src/script.js
+++ src/script.js
@@ -75,7 +75,8 @@ let month = months[monthLet.getMonth()];
 let monthElement = document.querySelector(".date-month");
 monthElement.innerHTML = `${date} of ${month} - ${year}`;
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast")
 
   let forecastHTML = `<div class="row">`;
@@ -95,11 +96,15 @@ function displayForecast() {
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;  
   })
-
-
 }
 
-displayForecast();
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "7efef5260931c8f50230e9ac708a39f6";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units="metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
+};
 
 // Temperature, city and country
 
@@ -134,7 +139,8 @@ function showTemp(response) {
       "alt", 
       response.data.weather[0].description
       );
-    
+
+  getForecast(response.data.coord);  
 
 }
 
